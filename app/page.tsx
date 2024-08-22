459dec39275eaf4f7ecb2e76a44fd87b380e0096
AliDurul <alidrl26@gmail.com>
2024-08-22 16:53:02 +0200

    chore: Add .env to .gitignore, update next-auth version, and add auth route and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { auth } from "@/auth";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
+
   return (
     <>
-      <h1>hi there</h1>
+      <h1 className="text-lime-600 text-2xl text-center">hi there</h1>
     </>
   );
 }
---
+++ auth.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth"
+import credentials from "next-auth/providers/credentials"
+import Google from "next-auth/providers/google"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+        }),
+        
+    ],
+    callbacks: {
+
+    },
+    pages: { signIn: '/auth' },
+    secret: process.env.AUTH_SECRET,
+    session: { strategy: 'jwt', maxAge: 30 * 24 * 60 * 60 },
+})
