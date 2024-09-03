d2696efb3485f47ee705d358f1913ba0a1e2ed25
AliDurul <alidrl26@gmail.com>
2024-09-03 18:55:33 +0200

    refactor: Remove unused ToastProvider component from RootLayout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -22,11 +22,11 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
   return (
     <html lang="en" >
       <body className={inter.className} >
-        <ToastProvider>
+        {/* <ToastProvider> */}
           <SessionProvider session={session} refetchInterval={60 * 60 * 24 * 1}>
             {children}
           </SessionProvider>
-        </ToastProvider>
+        {/* </ToastProvider> */}
       </body>
     </html>
   );
